274d70311159db31aa67c61e5de9d77e226b6af0
Dmitry Zhogin <zhogin.dima@gmail.com>
2025-05-28 14:07:26 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # moonly_test
+
+### Repository consists of:
+- [graph.py](./graph.py) file with a graph export
+- [langgraph.json](./langgraph.json) file for "langgraph dev" configuration
+- [requirements.txt](./requirements.txt) with required python libraries
+- [.env](./.env) file with model name
+
+#### Before running the langgraph dev you need to pull qwen3:0.6b model using the command below: 
+```bash
+ollama pull qwen3:0.6b
+```
---
+++ graph.py
@@ -0,0 +1,59 @@
+from typing import TypedDict, List
+import os
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+
+from datetime import datetime
+
+__all__=['graph']
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage | AIMessage]
+
+@tool
+def get_current_time():
+    """Return the current UTC time in ISO‑8601 format.
+    Example → {"utc": "2025‑05‑21T06:42:00Z"}"""
+    return datetime
+
+tools = [get_current_time]
+
+
+llm = init_chat_model(os.environ['MODEL_NAME'], model_provider='ollama').bind_tools(tools)
+
+def llm_node(state: AgentState):
+    response = llm.invoke(state["messages"])
+    state['messages'] = state['messages'] + [response]
+    return state
+
+
+def router(state: AgentState):
+    last_msg = state["messages"][-1]
+    if isinstance(last_msg, AIMessage) and last_msg.tool_calls:
+        return "tools"
+    return "end"
+
+
+tool_node = ToolNode([get_current_time])
+
+def tools_node(state: AgentState):
+    result = tool_node.invoke(state)
+    return {
+        'messages': state['messages'] + result['messages']
+    }
+    
+
+builder = StateGraph(AgentState)
+
+builder.add_node('llm', llm_node)
+builder.add_node('tools', tools_node)
+
+builder.add_edge(START, 'llm')
+builder.add_edge('tools', 'llm')
+builder.add_conditional_edges('llm', router, {'tools':'tools', 'end': END})
+
+graph = builder.compile()
